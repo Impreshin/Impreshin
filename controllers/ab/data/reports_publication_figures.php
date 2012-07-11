9bdc3338def33384e94d79903e462d50159752ee
William Stam <awstam@gmail.com>
2012-07-11 11:34:27 +0200

    reports - publication figures

--- controllers/ab/data/reports_publication_figures.php
+++ controllers/ab/data/reports_publication_figures.php
@@ -158,6 +158,12 @@ class reports_publication_figures extends data {
 		$d = models\bookings::getAll_select($select,$where, "global_dates.publish_date ASC");
 
 
+		$blank = array(
+			"totals" => 0,
+			"cm"     => 0,
+			"records"=> 0,
+		);
+		$margin = 10; // %
 
 
 		$data = array();
@@ -168,11 +174,7 @@ class reports_publication_figures extends data {
 				$data[$year]= array();
 			}
 			if (!isset($data[$year][$month])){
-				$data[$year][$month]= array(
-					"totals"=>0,
-					"cm"=>0,
-					"records"=>0,
-				);
+				$data[$year][$month]= $blank;
 			}
 
 			$data[$year][$month]['totals'] = $data[$year][$month]['totals'] + $record['totalCost'];
@@ -186,21 +188,100 @@ class reports_publication_figures extends data {
 		foreach ($months as $month){
 			$r = array(
 				"month"=>$month['h'],
-				"data"=>array()
+				"data"=>array(),
+				"averages"=> $blank
 			);
+			$i_t = 0;
+			$i_c = 0;
+			$i_r = 0;
+			$totals = $blank;
 			foreach ($years as $year){
 				if ($year['s']){
+
+					$total = isset($data[$year['y']][$month['k']]['totals']) ? ($data[$year['y']][$month['k']]['totals']) : 0;
+					$cm= isset($data[$year['y']][$month['k']]['cm']) ? ($data[$year['y']][$month['k']]['cm']) : 0;
+					$records= isset($data[$year['y']][$month['k']]['records']) ? ($data[$year['y']][$month['k']]['records']) : 0;
+
+
+					$totals['totals'] = $totals['totals'] + $total;
+					$totals['cm'] = $totals['cm'] + $cm;
+					$totals['records'] = $totals['records'] + $records;
+
+					if (isset($data[$year['y']][$month['k']]['totals'])){
+						$i_t++;
+					}
+					if (isset($data[$year['y']][$month['k']]['cm'])){
+						$i_c++;
+					}
+					if (isset($data[$year['y']][$month['k']]['records'])){
+						$i_r++;
+					}
+
+
 					$r['data'][] = array(
 						"year"=>$year['y'],
-						"totals"=>isset($data[$year['y']][$month['k']]['totals'])?currency($data[$year['y']][$month['k']]['totals']):"",
-						"cm"=>isset($data[$year['y']][$month['k']]['cm'])?$data[$year['y']][$month['k']]['cm']:"",
-						"records"=>isset($data[$year['y']][$month['k']]['records'])?$data[$year['y']][$month['k']]['records']:"",
+						"totals"=>($total)? ($total):"",
+						"cm"=>($cm)?$cm:"",
+						"records"=>($records)?$records:"",
+						"d"=>array(
+							"totals"=>"",
+							"cm"=>"",
+							"records"=>""
+						)
 					);
 
 
 				}
 
 			}
+
+			$r['averages']['totals'] = ($i_t)?$totals['totals'] / $i_t: $totals['totals'];
+			$r['averages']['cm'] = ($i_c) ? $totals['cm'] / $i_c : $totals['cm'];
+			$r['averages']['records'] = ($i_r) ? $totals['records'] / $i_r : $totals['records'];
+
+
+
+			$ndata = array();
+			foreach ($r['data'] as $rec) {
+				$col = "totals";
+				$figs_c_totals = array(
+					 $r['averages'][$col] + ($r['averages'][$col] * ($margin / 100)),
+					 $r['averages'][$col] - ($r['averages'][$col] * ($margin / 100)),
+				);
+				if ($rec[$col]> $figs_c_totals[0]){
+					$rec['d'][$col] = "u";
+				} else if ($rec[$col] < $figs_c_totals[1] ){
+					$rec['d'][$col] = "d";
+				}
+				$col = "cm";
+				$figs_c_totals = array(
+					 $r['averages'][$col] + ($r['averages'][$col] * ($margin / 100)),
+					 $r['averages'][$col] - ($r['averages'][$col] * ($margin / 100)),
+				);
+				if ($rec[$col]> $figs_c_totals[0]){
+					$rec['d'][$col] = "u";
+				} else if ($rec[$col] < $figs_c_totals[1] ){
+					$rec['d'][$col] = "d";
+				}
+				$col = "records";
+				$figs_c_totals = array(
+					 $r['averages'][$col] + ($r['averages'][$col] * ($margin / 100)),
+					 $r['averages'][$col] - ($r['averages'][$col] * ($margin / 100)),
+				);
+				if ($rec[$col]> $figs_c_totals[0]){
+					$rec['d'][$col] = "u";
+				} else if ($rec[$col] < $figs_c_totals[1] ){
+					$rec['d'][$col] = "d";
+				}
+
+
+
+
+
+				$rec['totals']= ($rec['totals']) ? currency($rec['totals']) : "";
+				$ndata[] = $rec;
+			}
+			$r['data'] = $ndata;
 
 			$ret[] = $r;
 		}
