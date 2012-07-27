4ee4ee9f85134ad11c10861c1a946b104a250188
William Stam <awstam@gmail.com>
2012-07-27 10:48:43 +0200

    # history page layout a tad

--- ui/_js/plugins/FeedEk.js
+++ ui/_js/plugins/FeedEk.js
@@ -18,15 +18,17 @@
 		$('#' + idd).empty();
 		$.ajax({url:'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=' + def.MaxCount + '&output=json&q=' + encodeURIComponent(def.FeedUrl) + '&callback=?', dataType:'json', success:function (data) {
 			$('#' + idd).empty();
+			var last_date = "";
 			$.each(data.responseData.feed.entries, function (i, entry) {
 
 				var data = $(entry.content).last("<pre>").html();
 				data = "<pre>" + data +"</pre>";
-
-				if (def.ShowPubDate) {
-					pubdt = new Date(entry.publishedDate);
-					$('#' + idd).append('<h3 class="ItemDate">' + pubdt.toLocaleDateString() + '</h3>')
+				pubdt = new Date(entry.publishedDate);
+				pubdt = pubdt.toLocaleDateString()
+				if (last_date!= pubdt) {
+					$('#' + idd).append('<h3 class="ItemDate">' + pubdt + '</h3>')
 				}
+				last_date = pubdt;
 				if (def.ShowDesc)$('#' + idd).append('<div class="ItemContent">' + data + '</div>')
 			});
 			def.callBack();
