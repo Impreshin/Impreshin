e776a4e6cb220d1cabbb3017466c18f0bef675d0
William Stam <awstam@gmail.com>
2012-07-27 10:03:17 +0200

    history page

--- ui/front/_js/page_history.js
+++ ui/front/_js/page_history.js
@@ -4,7 +4,7 @@
 
 var whole_pane = $("#whole-area .scroll-pane").jScrollPane(jScrollPaneOptions).data("jsp");
 $('#divRss').FeedEk({
-	FeedUrl    :'https://github.com/WilliamStam/Press-Apps/commits/master.atom?login=WilliamStam&token=e0619b48df9ba55f7f131a360bdf3ee3',
+	FeedUrl    :'https://github.com/WilliamStam/Press-Apps/commits/master.atom?login=WilliamStam&token=e0619b48df9ba55f7f131a360bdf3ee3&r='+Math.random(),
 	MaxCount   :20,
 	ShowDesc   :true,
 	ShowPubDate:true,
