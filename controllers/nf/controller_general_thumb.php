ddf786b415fe91960c3e4fad7c8394360ebe40ec
William Stam <awstam@gmail.com>
2012-11-15 14:02:24 +0200

    NewsFiler <wip>

--- controllers/nf/controller_general_thumb.php
+++ controllers/nf/controller_general_thumb.php
@@ -49,7 +49,7 @@ class controller_general_thumb {
 			\Graphics::thumb($folder . $filename, $w, $h);
 
 		} else {
-			\Graphics::fakeImage(0, 0);
+			\Graphics::fakeImage(1, 1);
 		}
 
 		exit();
@@ -58,60 +58,6 @@ class controller_general_thumb {
 
 	}
 
-	public static function remove_white($thumb){
-		$img = imagecreatefrompng($thumb);
-		//find the size of the borders
-		$b_top = 0;
-		$b_btm = 0;
-		$b_lft = 0;
-		$b_rt = 0;
-
-//top
-		for (; $b_top < imagesy($img); ++$b_top) {
-			for ($x = 0; $x < imagesx($img); ++$x) {
-				if (imagecolorat($img, $x, $b_top) != 0xFFFFFF) {
-					break 2; //out of the 'top' loop
-				}
-			}
-		}
-
-//bottom
-		for (; $b_btm < imagesy($img); ++$b_btm) {
-			for ($x = 0; $x < imagesx($img); ++$x) {
-				if (imagecolorat($img, $x, imagesy($img) - $b_btm - 1) != 0xFFFFFF) {
-					break 2; //out of the 'bottom' loop
-				}
-			}
-		}
-
-//left
-		for (; $b_lft < imagesx($img); ++$b_lft) {
-			for ($y = 0; $y < imagesy($img); ++$y) {
-				if (imagecolorat($img, $b_lft, $y) != 0xFFFFFF) {
-					break 2; //out of the 'left' loop
-				}
-			}
-		}
-
-//right
-		for (; $b_rt < imagesx($img); ++$b_rt) {
-			for ($y = 0; $y < imagesy($img); ++$y) {
-				if (imagecolorat($img, imagesx($img) - $b_rt - 1, $y) != 0xFFFFFF) {
-					break 2; //out of the 'right' loop
-				}
-			}
-		}
-
-//copy the contents, excluding the border
-		$newimg = imagecreatetruecolor(imagesx($img) - ($b_lft + $b_rt), imagesy($img) - ($b_top + $b_btm));
-
-		imagecopy($newimg, $img, 0, 0, $b_lft, $b_top, imagesx($newimg), imagesy($newimg));
-
-//finally, output the image
-
-
-		imagejpeg($newimg, $thumb);
-	}
 
 
 
