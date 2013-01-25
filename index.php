0970d22339442a78fa6086b3a847d9819c4de84d
William Stam <awstam@gmail.com>
2013-01-25 14:49:21 +0200

    F3 issues

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 $f3 = require('lib/f3/base.php');
+ob_start('ob_gzhandler');
 $f3->route('GET /', function ($f3) {
 		phpinfo();
 	}
 );
 $f3->run();
 
+$t = ob_get_contents();
 
+
+
+ob_end_clean();
+
+echo $t;
 
 exit();
 
