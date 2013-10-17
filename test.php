07dc239e6e01598ba185703123e3fb9b3e9cd833
William Stam <awstam@gmail.com>
2013-10-17 15:29:24 +0200

    Newsfiler wip

--- test.php
+++ test.php
@@ -1,3 +1,25 @@
 <?php
-$t = date("Y-m-d",strtotime(date("Y")."-".date('m')."-".'1')) . " to " . date("Y-m-d",strtotime(date("Y")."-".date('m')."-".date('t'))) ;
-echo $t;
+require_once('config.default.inc.php');
+require_once('config.inc.php');
+
+require_once('inc/functions.php');
+
+$link = mysql_connect($cfg['DB']['host'], $cfg['DB']['username'], $cfg['DB']['password']);
+mysql_select_db($cfg['DB']['database'], $link);
+
+
+
+$sql = "SELECT GROUP_CONCAT(ID) AS ID FROM `global_users` WHERE email is null;";
+$result = mysql_query($sql, $link) or die(mysql_error());
+
+
+
+$r = array();
+$s = array();
+
+
+	while ($row = mysql_fetch_assoc($result)) {
+		$r[] = $row['ID'];		
+	}
+
+test_array($r); 
