1b4fb68a8ceb2bbb1c7e5e767d47ce68306b828e
William Stam <awstam@gmail.com>
2013-11-07 12:48:27 +0200

    Newsfiler wip

--- inc/functions.php
+++ inc/functions.php
@@ -185,6 +185,7 @@ function form_display(&$value) {
 }
 function form_write(&$value) {
 	if ($value){
+		iconv(mb_detect_encoding($value, mb_detect_order(), true), "UTF-8", $value);
 		$value = html_entity_decode($value);
 	}
 	
