3d1fb0c13c678acd728a17700944b8e1f8c41203
William Stam <awstam@gmail.com>
2014-08-04 17:58:29 +0200

    Tetris test

--- controllers/general.php
+++ controllers/general.php
@@ -101,6 +101,7 @@ class general {
 				"/ui/_js/plugins/jquery.autologout.js",
 				"/ui/_js/plugins/select2.js",
 				"/ui/_js/plugins/jquery.getData.js",
+				"/ui/_js/plugins/jquery.overlaps.js",
 				"/ui/fancybox/jquery.fancybox.pack.js",
 
 
