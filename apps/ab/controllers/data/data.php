ea0f24e541d487d254e7778c7fa313b1330d9800
William Stam <awstam@gmail.com>
2014-08-06 15:54:33 +0200

    AdBooker # Details pane now shows all the other bookings added to the page and placed (Tetris)

--- apps/ab/controllers/data/data.php
+++ apps/ab/controllers/data/data.php
@@ -98,10 +98,83 @@ class data {
 
 		$return['a'] = $allow;
 
+
+		//test_array($return); 
+		
+		$return['page_details'] = $this->page_info($return['page'],$return['pID'],$return['dID']);
+
 		return $GLOBALS["output"]['data'] = $return;
 	}
 
+	function page_info($page_nr,$pID,$dID){
+		
 
+		$user = $this->f3->get("user");
+		$userID = $user['ID'];
+
+		
+
+		$page = models\pages::getAll("page='$page_nr' AND global_pages.dID = '$dID' AND global_pages.pID='$pID'");
+
+		if (count($page)) {
+			$page = $page[0];
+
+		} else {
+			$page = models\pages::dbStructure();
+			$page['page'] = $page_nr;
+		}
+		$pageID = $page['ID'];
+		
+
+
+		$bookingsRaw = models\bookings::getAll("(ab_bookings.pID = '$pID' AND ab_bookings.dID='$dID') AND checked = '1' AND ab_bookings.deleted is null AND typeID='1' AND pageID='$pageID'", "client ASC");
+		$bookings = array();
+		$cm = 0;
+		$records = 0;
+		foreach ($bookingsRaw as $booking) {
+			//test_array($booking); 
+			$a = array();
+			$a['ID'] = $booking['ID'];
+			$a['client'] = $booking['client'];
+			$a['colour'] = $booking['colour'];
+			$a['colourLabel'] = $booking['colourLabel'];
+			$a['col'] = $booking['col'] + 0;
+			$a['cm'] = $booking['cm'] + 0;
+			$a['totalspace'] = $booking['totalspace'] + 0;
+			$a['pageID'] = $booking['pageID'];
+			$a['page'] = $booking['page'];
+			$a['material'] = $booking['material'];
+			$a['material_approved'] = $booking['material_approved'];
+			$a['material_status'] = $booking['material_status'];
+			//$a['material_file_filename'] = $booking['material_file_filename'];
+			$a['material_file_store'] = $booking['material_file_store'];
+			$a['x_offset'] = $booking['x_offset']?$booking['x_offset']+0:"";
+			$a['y_offset'] = $booking['y_offset']?$booking['y_offset']+0:"";
+
+			$bookings[] = $a;
+			if ($a['cm']) $cm = $cm + $a['totalspace'] + 0;
+			$records++;
+		}
+
+		$page['records']= $bookings;
+
+
+		$pageSize = $user['publication']['cmav'] * $user['publication']['columnsav'];
+		$totalAVspace = $pageSize;
+		$loading = ($cm) ? ($cm / $totalAVspace) * 100 : 0;
+		$loading = number_format($loading, 2);
+
+
+
+		$page['stats'] = array(
+			"cm"=>$cm,
+			"records" => $records,
+			"loading" => $loading
+		);
+		return $GLOBALS["output"]['data'] = $page;
+	}
+	
+	
 
 
 
