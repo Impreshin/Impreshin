221c72735ed77d23effcd2a5f9bb78d2f8af9bf9
William Stam <william@impreshin.com>
2015-12-08 10:03:07 +0200

    + Https redirect

--- redirect/index.php
+++ redirect/index.php
@@ -2,7 +2,7 @@
 require_once('../config.default.inc.php');
 require_once('../config.inc.php');
 
-$redirect = "https://".$cfg['HOST_DOMAIN'].$_SERVER['REQUEST_URI'];
+$redirect = "https://".$cfg['HOST_DOMAIN']."/".$_SERVER['REQUEST_URI'];
 header("HTTP/1.1 301 Moved Permanently");
 header("Location: $redirect");
 
