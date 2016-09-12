d039b785ae3f89e66bdb015ca4faeef7aa204fcf
William Stam <william@impreshin.com>
2016-09-12 12:32:51 +0200

    redirect changes for the mailbox section

--- redirect/index.php
+++ redirect/index.php
@@ -2,7 +2,14 @@
 require_once('../config.default.inc.php');
 require_once('../config.inc.php');
 
-$redirect = $cfg['HOST_DOMAIN_COMPLETE']."/".$_SERVER['REQUEST_URI'];
+if (isset($_GET['relative'])){
+	$redirect = $_GET['url'];
+} else {
+	$redirect = $cfg['HOST_DOMAIN_COMPLETE']."/".$_SERVER['REQUEST_URI'];
+}
+
+
+
 header("HTTP/1.1 301 Moved Permanently");
 header("Location: $redirect");
 
